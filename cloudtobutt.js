9ac4a52e6ecd9c968c29cf432ded07f1b295e847
Ben Kreeger <benjaminkreeger@gmail.com>
2015-04-24 16:17:22 -0500

    Update to replace a few other special cases of "cloud."

--- cloudtobutt.js
+++ cloudtobutt.js
@@ -6,7 +6,7 @@ function walk(node) {
 	// I stole this function from here:
 	// http://is.gd/mwZp7E
 	var child, next;
-	
+
 	switch (node.nodeType) {
 		case 1:  // Element
 		case 9:  // Document
@@ -26,11 +26,20 @@ function walk(node) {
 
 function handleText(textNode)  {
 	var v = textNode.nodeValue;
-	
+
     v = v.replace(/\bThe Cloud\b/g, "My Butt");
 	v = v.replace(/\bThe cloud\b/g, "My butt");
     v = v.replace(/\bthe Cloud\b/g, "my Butt");
 	v = v.replace(/\bthe cloud\b/g, "my butt");
-	
+
+	v = v.replace(/\bcloud computing\b/g, "butt farting");
+	v = v.replace(/\bCloud computing\b/g, "Butt farting");
+	v = v.replace(/\bCloud Computing\b/g, "Butt Farting");
+
+	v = v.replace(/\biCloud\b/g, "iButt");
+
+	v = v.replace(/\bcloud\b/g, "butt");
+	v = v.replace(/\bCloud\b/g, "Butt");
+
 	textNode.nodeValue = v;
 }
